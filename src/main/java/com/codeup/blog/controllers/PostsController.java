8ea2396ba743d3d7fae9f89b869e7c7aedfa3205
paulvaldez <valdezpaul00@gmail.com>
2017-11-02 10:59:37 -0500

    Completed the Spring controllers exercise

---
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -0,0 +1,34 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostsController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String id() {
+        return "view an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String create() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postCreate() {
+        return "create a new post";
+    }
+}
