0073ece88eecbc128c8098295392f59529cc6309
paulvaldez <valdezpaul00@gmail.com>
2017-11-06 16:08:55 -0600

    Completed the first 4 parts of the services exercise.

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -5,10 +5,7 @@ import com.codeup.blog.services.PostSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -39,12 +36,14 @@ public class PostsController {
     }
 
     @GetMapping("/posts/create")
-    public String create() {
-        return "view the form for creating a post";
+    public String create(Model vModel) {
+        vModel.addAttribute("post", new Post());
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String postCreate() {
-        return "create a new post";
+    public String postCreate(@ModelAttribute Post post) {
+        postSvc.save(post);
+        return "redirect:/posts";
     }
 }
