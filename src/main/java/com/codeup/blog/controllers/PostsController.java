c3473e678ab69c07b030884e5378b4ef4471a1f6
paulvaldez <valdezpaul00@gmail.com>
2017-11-06 11:33:08 -0600

    Created a services package under com.codeup.blog.
    --Added PostSvc file to it.
    
    --Added an @Autowired to the PostsController for it.
    
    --Added two methods to the Post.java

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -1,6 +1,8 @@
 package com.codeup.blog.controllers;
 
 import com.codeup.blog.models.Post;
+import com.codeup.blog.services.PostSvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,13 @@ import java.util.ArrayList;
 
 @Controller
 public class PostsController {
+
+    private final PostSvc postSvc;
+
+    @Autowired
+    public PostsController(PostSvc postSvc) {
+        this.postSvc = postSvc;
+    }
 
     @GetMapping("/posts")
     public String showAll(Model vmodel) {
--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -4,10 +4,14 @@ public class Post {
 
     private String title;
     private String body;
+    private Long id;
 
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
+    }
+
+    public Post(long l, String first_title, String s) {
     }
 
     public String getBody() {
@@ -25,4 +29,9 @@ public class Post {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
 }
