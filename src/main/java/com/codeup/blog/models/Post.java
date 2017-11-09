d238f0405a96226d1da57c43b38de73d7b1ea696
paulvaldez <valdezpaul00@gmail.com>
2017-11-09 15:20:08 -0600

    Added sources to the application.properties.
    Created a repository  directory and added a Posts and Users in it.
    
    Added the crud respository to the Post, PostSvc, PostsController and User files.

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,9 +1,21 @@
 package com.codeup.blog.models;
+import com.sun.tools.javah.Gen;
+import javax.persistence.*;
+@Entity
+//@Table(name="")
 public class Post {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    //By default, String properties will be mapped to VARCHAR columns, if we want a column of type TEXT, we can specify it like so:
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String body;
-
+    //    @OneToOne //post can only have one user_id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
@@ -27,7 +39,16 @@ public class Post {
     public void setTitle(String title) {
         this.title = title;
     }
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
+    }
+    public long getId(){
+        return id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
     }
 }
