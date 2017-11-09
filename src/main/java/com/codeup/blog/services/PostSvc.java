e6d8e7862c0082c58338553a5d6e4021f3348816
paulvaldez <valdezpaul00@gmail.com>
2017-11-09 15:07:09 -0600

    Added an edit to the posts controller.
    Created a edit html file inside of posts.
    
    Added a save method to the PostsSvc.

--- src/main/java/com/codeup/blog/services/PostSvc.java
+++ src/main/java/com/codeup/blog/services/PostSvc.java
@@ -1,18 +1,21 @@
 package com.codeup.blog.services;
 import com.codeup.blog.models.Post;
+import com.codeup.blog.repositories.PostsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
-
-@Service
+@Service //Step 1 Annotate class- Spring Boot can't create object
 public class PostSvc {
-    private List<Post> posts = new ArrayList<>();
-
-    public PostSvc() {
-        createPosts();
+    private List<Post> posts = new ArrayList<>(); //keep an array list of posts internally
+    //Autowire an instance of this interface
+    private PostsRepository postDao;
+    @Autowired
+    public PostSvc(PostsRepository postDao) { //constructor
+//        createPosts();
+        this.postDao = postDao;
     }
-    //returns ALL posts and takes place of SHOWALL in posts controller
-    public List<Post> findAll() {
+    public List<Post> findAll() {  //returns ALL posts and takes place of SHOWALL in posts controller
         return posts;
     }
     public Post save(Post post) {
@@ -26,18 +29,17 @@ public class PostSvc {
     private void createPosts() { // same data that was in SHOWALL in posts controller
         // create some post objects and add them to the posts list
         // with the save method
-        this.save(new Post(
-                1L, "First Title", "Description 1"
-        ));
-        this.save(new Post(
-                2L, "Second Title", "Description 2"
-        ));
-        this.save(new Post(
-                3L, "Third Title", "Description 3"
-        ));
-        this.save(new Post(
-                4L, "Fourth Title", "Description 4"
-        ));
+//        this.save(new Post(
+//                1L, "First Title", "Description 1"
+//        ));
+//        this.save(new Post(
+//                2L, "Second Title", "Description 2"
+//        ));
+//        this.save(new Post(
+//                3L, "Third Title", "Description 3"
+//        ));
+//        this.save(new Post(
+//                4L, "Fourth Title", "Description 4"
+//        ));
     }
-
 }
